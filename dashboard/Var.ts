2d74a9e8b8cdeef7134088c6849ba96a1052cbf4
Farrukh Hussain Balay <FarrukhHGithub>
2025-12-17 16:07:02 +0500

    all packages updated

--- dashboard/Var.ts
+++ dashboard/Var.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:5000"
+// export const BASE_URL = "http://localhost:5000"
 // export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000"
 // export const BASE_URL = "https://api.mountaintravels.site" 
-// export const BASE_URL = "https://api.mountaintravels.com" 
+export const BASE_URL = "https://api.mountaintravels.com" 
 
 // export const BASE_URL = "https://mountaintravels.site"
